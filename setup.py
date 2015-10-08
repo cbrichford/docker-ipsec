d5d4ac5c03185bb177376f1278fd10743e8ea88e
Christopher Brichford <chrisb+git@farmersbusinessnetwork.com>
2015-10-08 09:25:45 -0700

    Adding classifiers and bumping version number.

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
   name = 'docker-ipsec',
-  version = '0.1',
+  version = '0.2',
   description = 'Scripts to start/stop ipsec VPN tunnels while adding/removing iptables rules for docker networking.',
   author = 'Christopher Brichford',
   author_email = 'chrisb@farmersbusinessnetwork.com',
   license='Apache License 2.0',
   keywords = ['ipsec', 'docker'], # arbitrary keywords
-  classifiers = [],
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Information Technology',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: POSIX :: Linux',
+    'Topic :: Internet',
+    'Topic :: System :: Networking'
+  ],
   scripts=['docker_ipsec/docker-ipsec.py'],
   install_requires=['pyroute2', 'netaddr', 'python-iptables', 'ipsecparse'],
+  url='https://github.com/cbrichford/docker-ipsec/',
   packages=[
         'docker_ipsec'
     ],
