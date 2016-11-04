d678551eeb220d8d7d633f2afb75cbbfc3d5b5a2
Christopher Brichford <chrisb+git@farmersbusinessnetwork.com>
2016-11-03 21:42:54 -0700

    Major re-working to support docker network names.
    Users no longer need to figure out which bridge device docker
    is using for each network.
    
    Re-worked command line parser.
    
    Code is mostly PEP8 compliant now.

--- docker_ipsec/common.py
+++ docker_ipsec/common.py
@@ -1,163 +1,215 @@
-import socket
+import docker
 import pyroute2
 import netaddr
 import tempfile
 import iptc
 import json
 import subprocess
+import collections
 
 def toJSON(obj):
     return json.dumps(obj, ensure_ascii=True, sort_keys=True)
 
 COMMENT_PREFIX = 'docker_ipsec:'
 
+class DockerIPSecError(RuntimeError):
+    pass
+
+
 class IPSecInfoEntry:
-    def __init__(self, tableEntry):
-        attrs = dict(tableEntry.get('attrs', []))
-        if (len(attrs) == 0):
-            raise RuntimeError('Unable to get attrs from table entry: {0}'.format(toJSON(tableEntry)))
+    def __init__(self, table_entry):
+        attrs = dict(table_entry.get('attrs', []))
+        if len(attrs) == 0:
+            raise DockerIPSecError('Unable to get attrs from table entry: {0}'.format(toJSON(table_entry)))
         self.__dest = attrs.get('RTA_DST', None)
-        if (self.__dest is None):
-            raise RuntimeError('Unable to get destination from table entry: {0}'.format(toJSON(tableEntry)))
+        if self.__dest is None:
+            raise DockerIPSecError('Unable to get destination from table entry: {0}'.format(toJSON(table_entry)))
         
         self.__src = attrs.get('RTA_PREFSRC', None)
-        if (self.__dest is None):
-            raise RuntimeError('Unable to get source from table entry: {0}'.format(toJSON(tableEntry)))
+        if self.__src is None:
+            raise DockerIPSecError('Unable to get source from table entry: {0}'.format(toJSON(table_entry)))
 
         self.__gateway = attrs.get('RTA_GATEWAY', None)
-        if (self.__dest is None):
-            raise RuntimeError('Unable to get gateway from table entry: {0}'.format(toJSON(tableEntry)))
+        if self.__gateway is None:
+            raise DockerIPSecError('Unable to get gateway from table entry: {0}'.format(toJSON(table_entry)))
 
         self.__interfaceIndex = attrs.get('RTA_OIF', None)
-        if (self.__interfaceIndex is None):
-            raise RuntimeError('Unable to get output interface from table entry: {0}'.format(tableEntry))
+        if self.__interfaceIndex is None:
+            raise DockerIPSecError('Unable to get output interface from table entry: {0}'.format(table_entry))
         
-        self.__destLen = tableEntry.get('dst_len', None)
-        if (self.__destLen is None):
-            raise RuntimeError('Unable to get destination mask')
+        self.__destLen = table_entry.get('dst_len', None)
+        if self.__destLen is None:
+            raise DockerIPSecError('Unable to get destination mask')
 
-    def destCIDR(self):
+    def destination_cidr(self):
         return '{0}/{1}'.format(self.__dest, self.__destLen)
 
-    def outputInterfaceIndex(self):
+    def output_interface_index(self):
         return self.__interfaceIndex
 
-    def sourceIP(self):
+    def source_ip(self):
         return self.__src
 
 
 class IPSecInfo:
 
-    def __init__(self, ipsecTableIndex=220, ipRoute=None):
-        ipRoute = ipRoute if ipRoute is not None else pyroute2.IPRoute()
+    def __init__(self, ipsec_table_index=220, ip_route=None):
+        ip_route = ip_route if ip_route is not None else pyroute2.IPRoute()
 
-        ipSecRoutes = ipRoute.get_routes(table=ipsecTableIndex)
-        self.__entries = tuple(map(IPSecInfoEntry, ipSecRoutes))
+        routes = ip_route.get_routes(table=ipsec_table_index)
+        self.__entries = tuple(map(IPSecInfoEntry, routes))
 
     def entries(self):
         return self.__entries
 
 
-class DockerInfo:
+def route_table_entry_matches_ipsec_connection(ipsec_connection, entry:IPSecInfoEntry):
+    right_subnet = ipsec_connection.get('rightsubnet', None)
+    if right_subnet is None:
+        raise DockerIPSecError('Unable to determine rightsubnet for connection: {0}'.format(toJSON(ipsec_connection)))
+    right_ip_network = netaddr.IPNetwork(right_subnet)
+    entry_dest_ip_network = netaddr.IPNetwork(entry.destination_cidr())
+    return right_ip_network == entry_dest_ip_network
 
-    def __init__(self, dockerBridgeName='docker0', ipRoute=None):
-        ipRoute = ipRoute if ipRoute is not None else pyroute2.IPRoute()
 
-        dockerAddresses = ipRoute.get_addr(label=dockerBridgeName)
-        if (len(dockerAddresses) != 1):
-            raise RuntimeError('Expected 1 docker address, found {0}'.format(toJSON(dockerAddresses)))
+def ip_network_for_docker_network(client: docker.Client,
+                                  network_name: str):
+    networks = client.networks(names=(network_name,))
+    networks_count = len(networks)
+    if networks_count == 0:
+        raise DockerIPSecError('Docker network not found: {0}'.format(network_name))
+    if networks_count > 1:
+        names = map(lambda n: n['Name'], networks)
+        raise DockerIPSecError('More than one docker network found:\n{0}'.format('\n'.join(names)))
+    network = networks[0]
+    if network['Name'] != network_name:
+        raise DockerIPSecError('Docker network not found: {0}'.format(network_name))
+    network_id = network['Id']
+    ipam = network.get('IPAM', None)
+    if not isinstance(ipam, dict):
+        raise DockerIPSecError('Docker network does not contain IPAM info:{0} ({1})'.format(network_name, network_id))
+    ipam_configs = ipam.get('Config', None)
+    if not isinstance(ipam_configs, list):
+        raise DockerIPSecError('Docker network IPAM info does not contain Config: {0} ({1})'.format(network_name, network_id))
+    ipam_config = ipam_configs[0]
+    if not isinstance(ipam_config, dict):
+        raise DockerIPSecError(
+            'Docker network IPAM Config entry is not a dictionary: {0} ({1})'.format(network_name, network_id))
+    subnet_str = ipam_config.get('Subnet', None)
+    if not isinstance(subnet_str, str):
+        raise DockerIPSecError('Docker network IPAM config does not contain Subnet: {0} ({1})'.format(network_name, network_id))
 
-        dockerAddress = dockerAddresses[0]
+    gateway_str = ipam_config.get('Gateway', None)
+    if not isinstance(gateway_str, str):
+        raise DockerIPSecError('Docker network IPAM config does not contain Gateway: {0} ({1})'.format(network_name, network_id))
 
-        attrs = dockerAddress.get('attrs', None)
-        if (attrs is None):
-            raise RuntimeError('Enable to get attrs from docker address: {0}'.format(toJSON(dockerAddress)))
-        attrs = dict(attrs)
-        self.__ip = attrs.get('IFA_LOCAL', None)
-        if (self.__ip is None):
-            raise RuntimeError('Unable to get docker ip address: {0}'.format(toJSON(dockerAddress)))
+    return netaddr.IPNetwork(subnet_str).cidr
 
-        self.__ipLen = dockerAddress.get('prefixlen', None)
-        if (self.__ipLen is None):
-            raise RuntimeError('Unable to get docker net mask: {0}'.format(toJSON(dockerAddress)))
 
-        self.__net = netaddr.IPNetwork('{0}/{1}'.format(self.__ip, self.__ipLen))
-
-    def cidr(self):
-        return '{0}/{1}'.format(str(self.__net.network), self.__ipLen)
-
-def getInterfaceNameForIndex(interfaceIndex, ipRoute=None):
-    ipRoute = ipRoute if ipRoute is not None else pyroute2.IPRoute()
-    links = ipRoute.get_links(interfaceIndex)
-    if (len(links) != 1):
-        raise RuntimeError('Expected one link for interface index, found {0}'.format(links))
+def interface_name_for_index(interface_index, ip_route=None):
+    ip_route = ip_route if ip_route is not None else pyroute2.IPRoute()
+    links = ip_route.get_links(interface_index)
+    if len(links) != 1:
+        raise DockerIPSecError('Expected one link for interface index, found {0}'.format(links))
 
     link = links[0]
     name = dict(link.get('attrs', [])).get('IFLA_IFNAME', None)
     if (name is None):
-        raise RuntimeError('Unable to get interface name: {0}'.format(toJSON(links)))
+        raise DockerIPSecError('Unable to get interface name: {0}'.format(toJSON(links)))
     return name
 
-def installIPTablesRule(table, dockerBridgeName, virtualIP, outInterface, destCIDR, dockerCIDR):
+
+def install_iptables_rule(table: iptc.Table,
+                          ipsec_connection_name: str,
+                          ipsec_virtual_ip: str,
+                          output_interface: str,
+                          destination_cidr: str,
+                          src_cidr: str):
     rule = iptc.Rule()
-    rule.out_interface = outInterface
-    rule.src = dockerCIDR
-    rule.dst = destCIDR
+    rule.out_interface = output_interface
+    rule.src = src_cidr
+    rule.dst = destination_cidr
 
     target = rule.create_target('SNAT')
-    target.set_parameter('to-source', virtualIP)
+    target.set_parameter('to-source', ipsec_virtual_ip)
 
     comment = rule.create_match('comment')
-    commentDict = {
-        'vip' : virtualIP,
-        'destCIDR' : destCIDR,
-        'dockerCIDR' : dockerCIDR,
-        'dockerBridgeName' : dockerBridgeName
+    comment_dict = {
+        'vip': ipsec_virtual_ip,
+        'destCIDR': destination_cidr,
+        'srcCIDR': src_cidr,
+        'connName': ipsec_connection_name
     }
-    commentJSONStr=json.dumps(commentDict, ensure_ascii=True, sort_keys=True, separators=(',', ':'))
-    comment.set_parameter('comment', '{}{}'.format(COMMENT_PREFIX, commentJSONStr))
-
+    comment_str=json.dumps(comment_dict, ensure_ascii=True, sort_keys=True, separators=(',', ':'))
+    comment.set_parameter('comment', '{}{}'.format(COMMENT_PREFIX, comment_str))
     chain = iptc.Chain(table, 'POSTROUTING')
-
     chain.insert_rule(rule)
-    
-def removeIPTablesRules(filterFunc=None):
-    def _commentFilter(c):
+
+
+def comment_matches_ip_network(ip_network: netaddr.IPNetwork, comment: dict):
+    src_cidr = comment.get('srcCIDR', None)
+    if src_cidr is None:
+        return False
+    src_network = netaddr.IPNetwork(src_cidr)
+    return src_network == ip_network
+
+
+def comment_matches_ipsec_connection(connection_name: str, comment: dict):
+    rule_connection_name = comment.get('connName', None)
+    if rule_connection_name is None:
+        return False
+    return rule_connection_name == connection_name
+
+
+def filter_iptables_rules(iptables_chain: iptc.Chain,
+                          filter_func):
+    def _comment_filter(c):
         if c.startswith(COMMENT_PREFIX):
-            if filterFunc is None:
-                return True
             try:
-                return filterFunc(json.loads(c[len(COMMENT_PREFIX):]))
+                return filter_func(json.loads(c[len(COMMENT_PREFIX):]))
             except ValueError:
                 return False
         return False
+
+    def _rule_filter(r):
+        matches = r.matches
+        matches_entries = tuple(map(lambda m: (m.name, m.get_all_parameters()), matches))
+        match_dict = dict(matches_entries)
+        comment = match_dict.get('comment', None)
+        if comment is None:
+            return False
+        comment_values = comment.get('comment', '')
+        matched_comments = tuple(filter(_comment_filter, comment_values))
+        if len(matched_comments) == 0:
+            return False
+        return True
+
+    return tuple(filter(_rule_filter, iptables_chain.rules))
+
+
+def remove_iptables_rules(filter_func=None):
     table = iptc.Table(iptc.Table.NAT)
     table.autocommit = False
     chain = iptc.Chain(table, 'POSTROUTING')
-    for r in chain.rules:
-        matches = r.matches
-        matchesEntries = tuple(map(lambda m: (m.name, m.get_all_parameters()), matches))
-        matchDict = dict(matchesEntries)
-        comment = matchDict.get('comment', None)
-        if (comment is None):
-            continue
-        commentValues = comment.get('comment', '')
-        matchedComments = tuple(filter(_commentFilter, commentValues))
-        if (len(matchedComments) == 0):
-            continue
+    if filter_func is None:
+        filter_func = lambda _: True
+    rules_to_delete = filter_iptables_rules(chain, filter_func=filter_func)
+    for r in rules_to_delete:
         chain.delete_rule(r)
     table.commit()
 
-def ipsec(*args, verbose=False):
-    cmdLine = ['ipsec']
-    cmdLine.extend(args)
+
+IPSecResult = collections.namedtuple('IPSecResult', ['status', 'output'])
+
+
+def ipsec(*args) -> IPSecResult:
+    cmd_line = ['ipsec']
+    cmd_line.extend(args)
         
-    with tempfile.TemporaryFile() as outputFile:
-        statusCode = subprocess.call(cmdLine, stdin=subprocess.DEVNULL, stdout=outputFile, stderr=outputFile)
-        if (verbose or statusCode != 0):
-            outputFile.seek(0)
-            outputStr = outputFile.read().decode("utf-8", "strict")
-            print(outputStr)
-        return statusCode == 0
+    with tempfile.TemporaryFile() as output_file:
+        status_code = subprocess.call(cmd_line, stdin=subprocess.DEVNULL, stdout=output_file, stderr=output_file)
+        output_file.seek(0)
+        output = output_file.read().decode("utf-8", "strict")
+        return IPSecResult(status=status_code, output=output)
     
 
--- docker_ipsec/docker-ipsec.py
+++ docker_ipsec/docker-ipsec.py
@@ -1,85 +1,223 @@
 #!/usr/bin/env python3
 
 
-import docker_ipsec
+from docker_ipsec import DockerIPSecError
+from docker_ipsec import ip_network_for_docker_network
+from docker_ipsec import interface_name_for_index
+from docker_ipsec import ipsec
+from docker_ipsec import IPSecInfo
+from docker_ipsec import IPSecInfoEntry
+from docker_ipsec import install_iptables_rule
+from docker_ipsec import route_table_entry_matches_ipsec_connection
+from docker_ipsec import comment_matches_ipsec_connection
+from docker_ipsec import filter_iptables_rules
+from docker_ipsec import remove_iptables_rules
 import ipsecparse
 import sys
-import pyroute2
+from pyroute2.iproute import IPRoute
 import iptc
 import argparse
+import functools
+import docker
+import netaddr
+
+
+def load_ipsec_conf(parsed_args):
+    try:
+        with open(parsed_args.ipsecConf, 'rt') as ipsec_conf_file:
+            ipsec_conf_str = ipsec_conf_file.read()
+    except BaseException as e:
+        raise DockerIPSecError('Failed to read: {0}'.format(parsed_args.ipsecConf)) from e
+    try:
+        return ipsecparse.loads(ipsec_conf_str)
+    except BaseException as e:
+        raise DockerIPSecError('Failed to parse: {0}'.format(parsed_args.ipsecConf)) from e
+
+
+def load_ipsec_connection(parsed_args):
+    ipsec_conf = load_ipsec_conf(parsed_args)
+    connection_name = parsed_args.connection
+    ipsec_connection = ipsec_conf.get(('conn', connection_name), None)
+    if ipsec_connection is None:
+        raise DockerIPSecError('Unable to find connection {0} in {1}'.format(connection_name,
+                                                                             parsed_args.ipsecConf))
+    ipsec_connection['name'] = connection_name
+    return ipsec_connection
+
+
+def detect_connection_name(parsed_args, ipsec_conf):
+    ipsec_connection_name = parsed_args.connection
+    if ipsec_connection_name is not None:
+        return ipsec_connection_name
+
+    connection_entries = map(lambda e: (e[0][0], e[1]),
+                             filter(lambda e: e[0][0] == 'conn' and e[0][1] != '%default',
+                                    ipsec_conf.entries()))
+    connections = dict(connection_entries)
+    if len(connections) != 1:
+        message_lines = ['IPSec configuration in {0} contains more than one connection, specify which one:'.format(parsed_args.ipsecConf)]
+        message_lines.extend(connections.keys())
+        message = '\n'.join(message_lines)
+        raise DockerIPSecError(message)
+    return tuple(connections.keys())[0]
+
+
+def ipsec_route_to_rule(ip_network: netaddr.IPNetwork, ip_route: IPRoute, e: IPSecInfoEntry):
+    ouptut_interface_index = e.output_interface_index()
+    output_interface = interface_name_for_index(ouptut_interface_index, ip_route=ip_route)
+    ip_network_cidr = '{0}/{1}'.format(ip_network.ip, ip_network.prefixlen)
+    return e.source_ip(), output_interface, e.destination_cidr(), ip_network_cidr
+
+
+def add_ip_networks(ip_route: IPRoute, ip_networks, ipsec_connection_name):
+    ipsec_info = IPSecInfo(ip_route=ip_route)
+    ipsec_entries = ipsec_info.entries()
+    rules = []
+
+    table = iptc.Table(iptc.Table.NAT)
+    table.autocommit = False
+    chain = iptc.Chain(table, 'POSTROUTING')
+
+    filter_func = functools.partial(comment_matches_ipsec_connection, ipsec_connection_name)
+    existing_rules = filter_iptables_rules(chain, filter_func)
+    existing_rules_sources = set(map(lambda er: netaddr.IPNetwork(er.src), existing_rules))
+
+    for network in ip_networks:
+        if network in existing_rules_sources:
+            continue
+        route_to_rule = functools.partial(ipsec_route_to_rule, network, ip_route)
+        rules.extend(map(route_to_rule, ipsec_entries))
+    if len(rules) > 0:
+        for rule in rules:
+            install_iptables_rule(table, ipsec_connection_name, *rule)
+        table.commit()
+
+
+def get_ipsec_connection_routes(ipsec_info: IPSecInfo,
+                                ipsec_connection):
+    filter_func = functools.partial(route_table_entry_matches_ipsec_connection,
+                                    ipsec_connection)
+    return tuple(filter(filter_func, ipsec_info.entries()))
+
+
+def is_connection_up(ip_route: IPRoute,
+                     ipsec_connection):
+    ipsec_info = IPSecInfo(ip_route=ip_route)
+    routes = get_ipsec_connection_routes(ipsec_info, ipsec_connection)
+    return len(routes) > 0
+
+
+def connection_up(parsed_args):
+    ipsec_connection = load_ipsec_connection(parsed_args)
+    connection_name = ipsec_connection['name']
+    docker_networks = parsed_args.dockerNetworks
+    if len(docker_networks) > 0:
+        docker_client = docker.Client()
+        docker_network_to_ip_network = functools.partial(ip_network_for_docker_network, docker_client)
+        docker_ip_networks = tuple(map(docker_network_to_ip_network, docker_networks))
+    else:
+        docker_ip_networks = tuple()
+
+    ip_route = IPRoute()
+    if not is_connection_up(ip_route, ipsec_connection):
+        ipsec_result = ipsec('up', connection_name)
+        if ipsec_result.status != 0:
+            raise DockerIPSecError('Failed to connect VPN: {0}\n{1}'.format(connection_name, ipsec_result.output))
+
+    add_ip_networks(ip_route, docker_ip_networks, connection_name)
+
+
+def connection_down(parsed_args):
+    ipsec_connection = load_ipsec_connection(parsed_args)
+    connection_name = ipsec_connection['name']
+    ip_route = IPRoute()
+    if is_connection_up(ip_route, ipsec_connection):
+        ipsec_result = ipsec('down', connection_name)
+        if ipsec_result.status != 0:
+            raise DockerIPSecError('Failed to disconnect VPN: {0}\n{1}'.format(connection_name, ipsec_result.output))
+
+    filter_func = functools.partial(comment_matches_ipsec_connection, connection_name)
+    remove_iptables_rules(filter_func)
+
+
+def add_docker_networks(parsed_args):
+    ipsec_connection = load_ipsec_connection(parsed_args)
+    connection_name = ipsec_connection['name']
+    docker_networks = parsed_args.dockerNetworks
+
+    docker_client = docker.Client()
+    docker_network_to_ip_network = functools.partial(ip_network_for_docker_network, docker_client)
+    docker_network_to_ip_network = functools.partial(ip_network_for_docker_network, docker_client)
+    docker_ip_networks = tuple(map(docker_network_to_ip_network, docker_networks))
+    ip_route = IPRoute()
+    if not is_connection_up(ip_route, ipsec_connection):
+        raise DockerIPSecError('IPSec connection {0} is not connected!'.format(connection_name))
+
+    add_ip_networks(ip_route, docker_ip_networks, connection_name)
+
+
+def remove_docker_networks(parsed_args):
+    docker_networks = parsed_args.dockerNetworks
+    docker_client = docker.Client()
+    docker_network_to_ip_network = functools.partial(ip_network_for_docker_network, docker_client)
+    docker_ip_networks = set(map(docker_network_to_ip_network, docker_networks))
+
+    def filter_func(comment):
+        src_cidr = comment.get('srcCIDR', None)
+        if src_cidr is None:
+            return False
+        src_network = netaddr.IPNetwork(src_cidr)
+        return src_network in docker_ip_networks
+    remove_iptables_rules(filter_func=filter_func)
+
 
 def main():
     desc = 'Start and stop IPSec tunnels while allowing docker containers to route traffic down the tunnels'
     parser = argparse.ArgumentParser(description=desc, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('command', type=str, choices=set(('up', 'down', 'addbridge', 'removebridge')),
-                        help='Start or stop an IPSec tunnel')
-
-    parser.add_argument('connection', type=str, default='')
-
-    parser.add_argument('--docker-bridge', dest='dockerBridge', type=str, default='docker0',
-                        help='Name of the docker bridge')
     parser.add_argument('--ipsec-route-table', dest='ipsecRouteTable', type=int, default=220,
                         help='Route table containing IPSec routes')
     parser.add_argument('--ipsec-conf', dest='ipsecConf', type=str, default='/etc/ipsec.conf',
                         help='IPSec configuration file')
-    
-    parsedArgs = parser.parse_args()
 
-    with open(parsedArgs.ipsecConf, 'rt') as ipsecConfFile:
-        ipsecConfStr = ipsecConfFile.read()
+    sub_parsers = parser.add_subparsers(dest='command')
 
-    ipsecConnectionName = parsedArgs.connection
-    if (ipsecConnectionName == ''):
-        ipsecConf = ipsecparse.loads(ipsecConfStr)
-        ipsecConnectionEntries = map(lambda e: (e[0][0], e[1]),
-                                    filter(lambda e: e[0][0] == 'conn' and e[0][1] != '%default',
-                                           ipsecConf.entries()))
-        ipsecConnections = dict(ipsecConnectionEntries)
-        if (len(ipsecConnections) != 1):
-            print('IPSec configuration in {0} contains more than one connection, specify which one:')
-            for c in ipsecConnections.keys():
-                print(c + '\n')
-            return 1
+    up_parser = sub_parsers.add_parser('up', help='Start specified vpn connection.')
+    up_parser.add_argument('connection',
+                           type=str,
+                           help='IPSec connection to bring up.')
+    up_parser.add_argument('-n',
+                           '--docker-network',
+                           dest='dockerNetworks',
+                           action='append',
+                           default=list(),
+                           help='Docker network to connect to the VPN.')
 
-        ipsecConnectionName = tuple(ipsecConnections.keys())[0]
+    down_parser = sub_parsers.add_parser('down', help='Stop specified vpn connection.')
+    down_parser.add_argument('connection', type=str)
 
-    if (parsedArgs.command == 'down'):
-        docker_ipsec.removeIPTablesRules()
-        if (not docker_ipsec.ipsec('down', ipsecConnectionName, verbose=True)):
-            return 1
-        return 0
+    add_network_parser = sub_parsers.add_parser('add-network',
+                                                help='Add iptables rules to route packets from docker network to vpn.')
+    add_network_parser.add_argument('connection', type=str)
+    add_network_parser.add_argument('dockerNetworks', nargs='+')
 
-    if (parsedArgs.command == 'removebridge'):
-        def _removalFunc(j):
-            try:
-                return j['dockerBridgeName'] == parsedArgs.dockerBridge
-            except:
-                return False
-        docker_ipsec.removeIPTablesRules(filterFunc=_removalFunc)
-        return 0
+    remove_network_parser = sub_parsers.add_parser('remove-network',
+                                                   help='Remove iptables rules that route packets from docker network to vpn.')
+    remove_network_parser.add_argument('dockerNetworks', nargs='+')
 
-    ipRoute = pyroute2.IPRoute()
-    dockerInfo = docker_ipsec.DockerInfo(ipRoute=ipRoute, dockerBridgeName=parsedArgs.dockerBridge)
+    parsed_args = parser.parse_args()
 
-    if parsedArgs.command == 'up' and not docker_ipsec.ipsec('up', ipsecConnectionName, verbose=True):
-        return 1
-
-    assert parsedArgs.command in ['up', 'addbridge']
-
-    ipsecInfo = docker_ipsec.IPSecInfo(ipRoute=ipRoute, ipsecTableIndex=parsedArgs.ipsecRouteTable)
-
-    def ipsecEntryToIPTablesRule(e):
-        outputInterfaceIndex = e.outputInterfaceIndex()
-        outputInterface = docker_ipsec.getInterfaceNameForIndex(outputInterfaceIndex, ipRoute=ipRoute)
-        return (e.sourceIP(), outputInterface, e.destCIDR(), dockerInfo.cidr())
-
-    rules = tuple(map(ipsecEntryToIPTablesRule, ipsecInfo.entries()))
-    table = iptc.Table(iptc.Table.NAT)
-    table.autocommit = False
-    for rule in rules:
-        docker_ipsec.installIPTablesRule(table, parsedArgs.dockerBridge, *rule)
-    table.commit()
+    try:
+        if parsed_args.command == 'up':
+            connection_up(parsed_args)
+        elif parsed_args.command == 'down':
+            connection_down(parsed_args)
+        elif parsed_args.command == 'add-network':
+            add_docker_networks(parsed_args)
+        elif parsed_args.command == 'remove-network':
+            remove_docker_networks(parsed_args)
+    except DockerIPSecError as e:
+        print(e.args[0])
 
 if __name__ == '__main__':
     sys.exit(main())
